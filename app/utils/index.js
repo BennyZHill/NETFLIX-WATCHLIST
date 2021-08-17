bad757e81a0de39de9ea38f162689deedd0c1bf4
BennyZHill <ben.h@builtbypixel.com>
2021-08-17 14:18:53 +0100

    added exports

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
-exports.createMovie = (id, name, year, director) => {
+exports.createMovie = (id, name, year, director, restriction, genre) => {
   const movie = new movie({
     id: id,
     name: name,
     year: year,
     director: director,
+    restriction: restriction,
+    genre: genre,
   });
   console.log(movie);
 };
